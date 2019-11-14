152e95f0ba873e6cefdcf45fa727191344d5e54c
spno77 <spiro@spiro.com>
2019-11-14 13:48:31 +0200

    second program addition

---
+++ char_occur.py
@@ -0,0 +1,18 @@
+
+#occurencies of a character
+def char_occur(char,str):
+	occurrencies = str.count(char)
+	#print(occurrencies)
+	return occurrencies
+
+str = input("Enter a string:" )
+
+#tests
+count = char_occur('p',str)
+print("char p occured {} times in the string".format(count))
+
+count = char_occur('n',str)
+print("char n occured {} times in the string".format(count))
+
+count = char_occur('z',str)
+print("char z occured {} times in the string".format(count))
